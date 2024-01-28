1ef40491dab4315224d96a8267c924db167bc122
Alexander Schlenk <alexander@schlenk.at>
2024-01-29 00:43:00 +0100

    wip

--- app/src/main/java/com/example/android/politicalpreparedness/network/CivicsHttpClient.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/network/CivicsHttpClient.kt
@@ -1,13 +1,13 @@
 package com.example.android.politicalpreparedness.network
 
+import com.example.android.politicalpreparedness.BuildConfig
 import okhttp3.OkHttpClient
 
 class CivicsHttpClient : OkHttpClient() {
 
     companion object {
 
-        const val API_KEY =
-            ""
+        const val API_KEY = BuildConfig.apiKey
 
         fun getClient(): OkHttpClient {
             return Builder()
